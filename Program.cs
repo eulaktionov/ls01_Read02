8f0580722018cf19c92cc18e39c51d18e83d2b4e
eugeneLuck <eugeneLuck@gmail.com>
2023-02-25 14:05:44 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Data.SqlClient;
+using static System.Console;
+
+WriteLine("ExecuteReader");
+
+SqlConnection connection = null;
+SqlDataReader reader = null;
+
+try
+{
+    string connectionString =
+        @"Data Source=(localdb)\MSSQLLocalDB;" +
+        "Initial Catalog=Library01;";
+    connection = new SqlConnection(connectionString);
+    connection.Open();
+
+    string commandText =
+        "select * " +
+        "from Authors";
+    SqlCommand command =
+        new SqlCommand(commandText, connection);
+    reader = command.ExecuteReader();
+    ShowData();
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    if (reader != null)
+    {
+        reader.Close();
+    }
+    if(connection is not null)
+    {
+        connection.Close();
+    }
+    ReadKey();
+}
+
+void ShowData()
+{
+    for(int i = 0; i < reader.FieldCount; i++)
+    {
+        Write($"{reader.GetName(i), 20}");
+    }
+    WriteLine();
+    while(reader.Read())
+    {
+        for(int i = 0; i < reader.FieldCount; i++)
+        {
+            Write($"{reader[i],20}");
+        }
+        WriteLine();
+    }
+}
+
